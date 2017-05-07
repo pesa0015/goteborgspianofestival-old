556006af9e0de14f02ba09315b06b71f9ed84da8
pesa0015 <pesa0015@gmail.com>
2017-05-07 17:40:18 +0200

    Move wp_footer() before </body>

--- footer.php
+++ footer.php
@@ -1,6 +1,6 @@
 	</div>
     <script>var lang = '<?php echo $_SESSION['lang']; ?>';</script>
     <script src="<?php bloginfo('template_url'); ?>/js/script.min.js"></script>
+    <?php wp_footer(); ?>
 </body>
 </html>
-<?php wp_footer(); ?>
