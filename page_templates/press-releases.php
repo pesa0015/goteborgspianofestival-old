3016aab40eef4d8300f8605dda66082b6ccb7026
pesa0015 <pesa0015@gmail.com>
2017-09-06 22:09:58 +0200

    Get lang for press releases

--- page_templates/press-releases.php
+++ page_templates/press-releases.php
@@ -3,11 +3,13 @@
 $category = get_category_by_slug('pressmeddelanden');
 $releases = get_posts(array('cat' => $category->term_id, 'numberposts' => 0));
 
+$lang = (is_page('english')) ? 'en' : 'sv';
+
 foreach ($releases as $release) :
     $id = $release->ID;
     $date = $release->post_date;
-    $title = get_field('title_sv', $id);
-    $content = get_field('body_sv', $id); ?>
+    $title = get_field('title_' . $lang, $id);
+    $content = get_field('body_' . $lang, $id); ?>
     <div class="release">
         <hr />
         <span class="release-date"><?php echo ucfirst(date_i18n('j F, Y', strtotime($date))); ?></span>
