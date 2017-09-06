988a595954004a2059089a67dc2f8771fe021a43
pesa0015 <pesa0015@gmail.com>
2017-09-06 21:58:18 +0200

    Fix wrong date

--- page_templates/press-releases.php
+++ page_templates/press-releases.php
@@ -5,11 +5,12 @@ $releases = get_posts(array('cat' => $category->term_id, 'numberposts' => 0));
 
 foreach ($releases as $release) :
     $id = $release->ID;
+    $date = $release->post_date;
     $title = get_field('title_sv', $id);
     $content = get_field('body_sv', $id); ?>
     <div class="release">
         <hr />
-        <span class="release-date"><?php echo get_the_date(); ?></span>
+        <span class="release-date"><?php echo ucfirst(date_i18n('j F, Y', strtotime($date))); ?></span>
         <br />
         <br />
         <span class="release-title"><?php echo $title; ?></span>
