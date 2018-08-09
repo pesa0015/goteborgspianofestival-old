7c53bf6addbe4131641cb84e02d22f8afb0945e7
pesa0015 <pesa0015@gmail.com>
2018-08-09 23:10:31 +0200

    Move partners to sponsors page

--- index.php
+++ index.php
@@ -38,17 +38,6 @@ $translate = getTranslations();
     </div>
 </div>
 <div id="footer">
-    <h1 id="partners-title" class="title">Samarbetspartners</h1>
-    <div id="partners">
-        <span class="partner">Göteborgs konserthus</span>
-        <span class="dot"></span>
-        <span class="partner">GöteborgsOperan</span>
-        <span class="dot"></span>
-        <span class="partner">Svenska Balettskolan</span>
-        <span class="dot"></span>
-        <span class="partner">Dicksonska palatset</span>
-    </div>
-    <img src="<?php bloginfo('template_url'); ?>/img/svenska_balettskolan.png" alt="" style="height: 200px; display: block; margin: 70px auto;">
     <div id="created-by">
         <span>
             <span id="created-by-title">Sidan skapades av</span>
--- page-sponsors.php
+++ page-sponsors.php
@@ -8,4 +8,15 @@ get_header();
         <?php get_template_part('page_templates/sponsors', 'sponsors'); ?>
     </div>
 </div>
+<h1 id="partners-title" class="title">Samarbetspartners</h1>
+<div id="partners">
+    <span class="partner">Göteborgs konserthus</span>
+    <span class="dot"></span>
+    <span class="partner">GöteborgsOperan</span>
+    <span class="dot"></span>
+    <span class="partner">Svenska Balettskolan</span>
+    <span class="dot"></span>
+    <span class="partner">Dicksonska palatset</span>
+</div>
+<img src="<?php bloginfo('template_url'); ?>/img/svenska_balettskolan.png" alt="" style="height: 200px; display: block; margin: 70px auto;">
 <?php get_footer(); ?>
